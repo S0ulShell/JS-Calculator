b58157060014b32f82fe1525662cf65c97ba911e
S0ulShell <rajatpkumar24@gmail.com>
2024-04-08 14:43:38 +1200

    created for loop that creats buttons for the calc, assigning whats on the button as the class name

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+let num1;
+let operator;
+let num2;
+var digits = ["c","?","?","%",7,8,9,"X",4,5,6,"+",1,2,3,"-",0,".","Del", "="];
+
+for(i = 0; i < digits.length; i++) {
+    var button = document.createElement("button");
+    button.innerHTML = digits[i];
+    button.className = digits[i];
+    var buttonContainer = document.getElementById("buttons");
+    buttonContainer.appendChild(button);
+  }
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -13,3 +26,21 @@ function multiply(num1, num2){
 function divide(num1, num2){
     return num1 / num2;
 }
+
+function operate(num1, operator, num2) {
+    switch(operator){
+        case "+":
+            add(num1, num2);
+            break;
+        case "-":
+            subtract(num1, num2);
+            break;
+        case "*":
+            multiply(num1, num2);
+            break;
+        case "/":
+            divide(num1, num2);
+            break;
+    }
+}
+
